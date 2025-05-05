5c6cf2a5f4af7ceea735d90ee4654a73284e67bd
luisbanez <luismbanez@gmail.com>
2025-05-05 22:12:21 +0800

    fix basic usage examples and typos

--- include/lvec.h
+++ include/lvec.h
@@ -1,5 +1,5 @@
 /*
-  list.c is a simple generic dynamic array implementation.
+  lvec.c is a simple generic dynamic array implementation.
 
   NOTE: Use of this library requires 'typeof' support.
 
@@ -7,11 +7,11 @@
 
       int main()
       {
-          int* nums = list_new(int);
+          int* nums = lvec_new(int);
 
-          list_append(nums, 10);
-          list_append(nums, 20);
-          for (size_t i = 0; i < list_get_length(nums); i++)
+          lvec_append(nums, 10);
+          lvec_append(nums, 20);
+          for (size_t i = 0; i < lvec_get_length(nums); i++)
           {
               printf("%d", nums[i]);
           }
@@ -19,7 +19,7 @@
           free(nums);
       }
 
-  Make sure to use the *_aggregate functions when appending and inserting to a list
+  Make sure to use the *_aggregate functions when appending and inserting to an lvec
   of structs or unions.
 
       typedef struct Vector2
@@ -30,13 +30,13 @@
 
       int main()
       {
-          Vector2* vecs = list_new(Vector2);
+          Vector2* vecs = lvec_new(Vector2);
 
           Vector2 vec1 = { 0, 1 };
-          list_append_aggregate(vecs, vec1);
+          lvec_append_aggregate(vecs, vec1);
 
           Vector2 vec2 = { 3, 6 };
-          list_append_aggregate(vecs, vec2);
+          lvec_append_aggregate(vecs, vec2);
 
           free(nums);
       }
@@ -45,10 +45,10 @@
 
       int main()
       {
-          int* nums = list_new(int);
+          int* nums = lvec_new(int);
 
-          ListResult result = list_append(nums, 20);
-          if (result != LISTRESULT_SUCCESS)
+          LVecResult result = lvec_append(nums, 20);
+          if (result != LVECRESULT_SUCCESS)
           {
               // handle error
           }
@@ -61,15 +61,15 @@
 #ifndef LVEC_H
 #define LVEC_H
 
-// allocated memory for a new list
+// allocates memory for a new lvec
 #define lvec_new( T ) \
     _lvec_new( sizeof( T ) )
 
-// appends an element to the end of the list (use this for primitive types and enums)
+// appends an element to the end of the lvec (use this for primitive types and enums)
 #define lvec_append( lvec, data ) \
     _lvec_append( ( void** )&( lvec ), &( typeof( *( lvec ) ) ){ ( data ) } )
 
-// appends an element to the end of the list (use this for structs and unions)
+// appends an element to the end of the lvec (use this for structs and unions)
 #define lvec_append_aggregate( lvec, data ) \
     _lvec_append( ( void** )&( lvec ), &( data ) )
 
@@ -85,19 +85,19 @@
 #define lvec_remove( lvec, index ) \
     _lvec_remove( ( void** )&( lvec ), ( index ) )
 
-// removes the last element in the list
+// removes the last element in the lvec
 #define lvec_remove_last( lvec ) \
-    _lvec_remove( ( void** )&( lvec ), list_get_length( lvec ) - 1 )
+    _lvec_remove( ( void** )&( lvec ), lvec_get_length( lvec ) - 1 )
 
 // allocates enough memory for at least the specified capacity
 #define lvec_reserve_minimum( lvec, desired_capacity ) \
     _lvec_reserve_minimum( ( void** )&( lvec ), ( desired_capacity ) )
 
-// resizes array to match the length of the list
+// resizes array to match the length of the lvec
 #define lvec_shrink_to_fit( lvec ) \
     _lvec_shrink_to_fit( ( void** )&( lvec ) )
 
-// checks if length of list is 0
+// checks if length of lvec is 0
 #define lvec_is_empty( lvec ) \
     ( lvecget_length( lvec ) == 0 )
 
@@ -109,19 +109,19 @@ typedef enum LVecResult
     LVECRESULT_ERROR_ALLOCATION,
 } LVecResult;
 
-// allocates memory for a new list (use the macro version)
+// allocates memory for a new lvec (use the macro version)
 void* _lvec_new( size_t size );
 
-// deallocates memory for allocated list
+// deallocates memory for allocated lvec
 void lvec_free( void* lvec );
 
 // allocates enough memory for at least the specified capacity (use the macro version)
 LVecResult _lvec_reserve_minimum( void** lvec, size_t desired_capacity );
 
-// resizes array to match the length of the list (use the macro version)
+// resizes array to match the length of the lvec (use the macro version)
 LVecResult _lvec_shrink_to_fit( void** lvec );
 
-// appends an element to the end of the list (use macro version)
+// appends an element to the end of the lvec (use macro version)
 LVecResult _lvec_append( void** lvec, void* data );
 
 // appends an element at the specified index (use macro version)
@@ -130,13 +130,13 @@ LVecResult _lvec_insert( void** lvec, void* input_data, size_t index );
 // removes an element by index (use the macro version)
 LVecResult _lvec_remove( void** lvec, size_t index );
 
-// clears the list
+// clears the lvec
 void lvec_clear( void* lvec );
 
-// returns the number of items in the list
+// returns the number of items in the lvec
 size_t lvec_get_length( void* lvec );
 
-// returns how many elements can currently fit on the list
+// returns how many elements can currently fit on the lvec
 size_t lvec_get_capacity( void* lvec );
 
 #endif
